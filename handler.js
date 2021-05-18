7afa710d26bb9c2fa9431333d82b3f57cdcd7b45
Tudor <sekaru@sleepystudios.net>
2021-05-18 22:13:06 +0100

    Revert "become creepy"
    This reverts commit 349d8b58565297a03d7e29b7334ab82a593e48b3.

--- handler.js
+++ handler.js
@@ -2,21 +2,17 @@
 
 const Discord = require('discord.js')
 
-// const messages = [
-//   '🚨 Time to do some work',
-//   '⏰ Ring ring, it\'s work o\'clock',
-//   '📟 Beep beep, the 90\'s just called, they\'re saying you should open up Paint and do some work',
-//   '👋 Hey, it\'s your daily reminder to do some work',
-//   '🙏 Please do some work',
-//   '🌙 Time for some indie video game development',
-//   '🎮 Do some work on whatever game we\'re working on',
-//   '📝 Time to tick something off the to-do list',
-//   '🦾 Stretch those finger muscles, do some pixel art',
-//   '🎨 Time to boot up Paint and get to work'
-// ]
-
 const messages = [
-  'hello darrel, it is me, your good friend mrala, the latest hire at sleepy studios. it has come to my attention that you are once again slacking. please rate some games tonight. lots of love.'
+  '🚨 Time to do some work',
+  '⏰ Ring ring, it\'s work o\'clock',
+  '📟 Beep beep, the 90\'s just called, they\'re saying you should open up Paint and do some work',
+  '👋 Hey, it\'s your daily reminder to do some work',
+  '🙏 Please do some work',
+  '🌙 Time for some indie video game development',
+  '🎮 Do some work on whatever game we\'re working on',
+  '📝 Time to tick something off the to-do list',
+  '🦾 Stretch those finger muscles, do some pixel art',
+  '🎨 Time to boot up Paint and get to work'
 ]
 
 module.exports.alarm = async () => {
